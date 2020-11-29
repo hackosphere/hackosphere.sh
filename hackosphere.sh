c0f9eaa670642888b8b18de1f90de33147ca6598
Udit Patel <uditatulpatel@gmail.com>
2020-11-29 14:25:49 +0530

    added ssl

--- hackosphere.sh
+++ hackosphere.sh
@@ -3,6 +3,20 @@
 echo please enter your website URL:
 read websiteURL
 
+echo add ssl
+read YesNo
+
 wget -qO ee hackosphere.sh && sudo bash ee
 
+if [ $YesNo === "y" || $YesNo === "Y" ]; then
+
+echo enter you Email:
+read email
+ee site create example.com --type=wp --ssl=le
+$email
+
+else
+
 sudo ee site create $websiteURL --type=wp
+
+
